2d46d79a16f3b69001378419163f8538ec3d286c
Sergio Vidal <s.vidal.bravo.92@gmail.com>
2024-08-21 20:52:30 -0400

    se ordena el codigo para que la multiplicacion se muestre antes de las factoriales

--- assets/factorial.js
+++ assets/factorial.js
@@ -1,43 +1,30 @@
 const numeroSolicitado = () => {
-    let numero = prompt("ingrese un numero entre 1 y 20");
-    numero = parseInt(numero);
-    
-    if (numero >= 1  && numero <=20 ) {
+  let numero = prompt("ingrese un numero entre 1 y 20");
+  numero = parseInt(numero);
 
-        calculoFactorial(numero);
-
-
-    }
-    else {
-        console.log("ingrese un numero solamente entre 1 y 20");
+  if (numero >= 1 && numero <= 20) {
+    for (let i = 1; i <= numero; i++) {
+      let multiplicacion = i * numero;
+      console.log("la multiplicacion es: ", multiplicacion);
     }
 
+    calculoFactorial(numero);
+  } else {
+    console.log("ingrese un numero solamente entre 1 y 20");
+  }
+};
 
+const calculoFactorial = (num) => {
+  for (let i = 1; i <= num; i++) {
+    let factorial = 1;
 
-}
+    //console.log(`${i} x ${num} = ${i * num}`);
 
-const calculoFactorial= (num) => {
-
-   
-
-    for(let i =1; i <= num; i++) {
-
-        let factorial = 1;
-
-        console.log(`${i} x ${num} = ${i * num}`);
-
-        for(let j = 1; j <= i; j++) {
-        
-            factorial *= j;
-            
-        }
-        console.log(`el factorial de ${i} es: ${factorial}`); 
-
+    for (let j = 1; j <= i; j++) {
+      factorial *= j;
     }
-}
-
-
-
+    console.log(`el factorial de ${i} es: ${factorial}`);
+  }
+};
 
 numeroSolicitado();
-
